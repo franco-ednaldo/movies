e239ca5f86000b28629ca892935fb052ce1f9b35
Ednaldo Franco <ednaldofranco@MacBook-Air-de-Ednaldo.local>
2024-12-17 23:14:33 -0300

    First Commit

---
+++ MoviesCurso/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesCurso
+//
+//  Created by Ednaldo Franco on 17/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
